03ac0a0b18b95e0b1b5229cf10bd8907e1b2b87f
Alex <beichuanalex@gmail.com>
2023-06-04 11:28:55 +0800

    修改表结构和添加初始化数据脚本

--- Initial-sql-script/lemon.sql
+++ Initial-sql-script/lemon.sql
@@ -1,56 +1,62 @@
 /*==============================================================*/
 /* DBMS name:      PostgreSQL 9.x                               */
-/* Created on:     2023/6/2 3:56:36                             */
+/* Created on:     2023/6/4 11:22:46                            */
 /*==============================================================*/
 
 
 /*==============================================================*/
-/* Table: acct_type                                             */
+/* Table: detail_category                                       */
 /*==============================================================*/
-create table acct_type (
+create table detail_category (
    id                   BIGSERIAL            not null,
-   type                 INT2                 not null,
+   detail_type          INT2                 not null,
    icon                 VARCHAR(255)         not null,
    icon_name            VARCHAR(255)         not null,
    sort                 INT8                 not null,
    create_time          TIMESTAMP WITH TIME ZONE not null,
    update_time          TIMESTAMP WITH TIME ZONE not null,
-   constraint PK_ACCT_TYPE primary key (id),
-   constraint AK_KEY_2_ACCT_TYP unique (type, sort)
+   constraint PK_DETAIL_CATEGORY primary key (id),
+   constraint AK_KEY_2_DETAIL_C unique (detail_type, sort)
 );
 
-comment on table acct_type is
-'记账类型表';
+comment on table detail_category is
+'记账明细分类表';
 
-comment on column acct_type.id is
+comment on column detail_category.id is
 'ID';
 
-comment on column acct_type.type is
-'记账类型 {"收入":1,"支出":2}';
+comment on column detail_category.detail_type is
+'明细类型 {"收入":1,"支出":2}';
 
-comment on column acct_type.icon is
+comment on column detail_category.icon is
 'Icon 图标';
 
-comment on column acct_type.icon_name is
+comment on column detail_category.icon_name is
 'Icon 名称';
 
-comment on column acct_type.sort is
+comment on column detail_category.sort is
 '排序序号';
 
-comment on column acct_type.create_time is
-'创建时间';
+comment on column detail_category.create_time is
+'创建时间 ( UTC )';
 
-comment on column acct_type.update_time is
-'更新时间';
+comment on column detail_category.update_time is
+'更新时间 ( UTC )';
 
 /*==============================================================*/
 /* Table: ledger_detail                                         */
 /*==============================================================*/
 create table ledger_detail (
    id                   BIGSERIAL            not null,
-   acct_type_id         INT8                 not null,
+   detail_category_id   INT8                 not null,
+   detail_type          INT2                 not null,
    amount               NUMERIC(72,18)       not null,
    remark               VARCHAR(255)         null,
+   year                 INT2                 not null,
+   month                INT2                 not null,
+   day                  INT2                 not null,
+   week                 INT2                 not null,
+   date_number          INT4                 not null,
    create_time          TIMESTAMP WITH TIME ZONE not null,
    update_time          TIMESTAMP WITH TIME ZONE not null,
    constraint PK_LEDGER_DETAIL primary key (id)
@@ -62,8 +68,11 @@ comment on table ledger_detail is
 comment on column ledger_detail.id is
 'ID';
 
-comment on column ledger_detail.acct_type_id is
-'记账类型表 ID';
+comment on column ledger_detail.detail_category_id is
+'记账明细分类表 ID';
+
+comment on column ledger_detail.detail_type is
+'明细类型 ( detail_category 表的冗余字段 ) {"收入":1,"支出":2}';
 
 comment on column ledger_detail.amount is
 '金额';
@@ -71,9 +80,24 @@ comment on column ledger_detail.amount is
 comment on column ledger_detail.remark is
 '备注';
 
+comment on column ledger_detail.year is
+'创建时间 ( 冗余字段) [年]';
+
+comment on column ledger_detail.month is
+'创建时间 ( 冗余字段) [月]';
+
+comment on column ledger_detail.day is
+'创建时间 ( 冗余字段) [日]';
+
+comment on column ledger_detail.week is
+'创建时间 ( 冗余字段) [周]';
+
+comment on column ledger_detail.date_number is
+'创建时间 ( 冗余字段) [年月日]';
+
 comment on column ledger_detail.create_time is
-'创建时间';
+'创建时间 ( UTC )';
 
 comment on column ledger_detail.update_time is
-'更新时间';
+'更新时间 ( UTC )';
 
